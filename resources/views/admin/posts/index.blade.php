97f08c28d1cb93884d3c1445b97acaf94fab020b
NicholasDevelop <marcaccini.n@gmail.com>
2022-05-03 16:25:17 +0200

    Modificata pagina dell'index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,13 +2,44 @@
 
 @section('content')
 
-<table>
-    <th>Titolo</th>
-    @foreach($posts as $key => $post)
-        <tr>
-            <td>{{$post->title}}</td>
-        </tr>
-    @endforeach
-</table>
+<div class="container my-4">
+    <a class="btn btn-primary" href="{{ route('admin.posts.create') }}">Nuovo Post</a>
+</div>
+
+<div class="container">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Data Pubblicazione</th>
+            <th scope="col">Data Creazione</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $key => $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->slug }}</td>
+                <td>{{ $post->published_at }}</td>
+                <td>{{ $post->created_at }}</td>
+                <td>
+                    <a class="btn btn-small btn-warning" href="{{ route('admin.posts.edit', $post) }}">Edit</a>  
+                </td>
+                <td>
+                    <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Elimina</button>
+                    </form>
+                </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+</div>
 
 @endsection
